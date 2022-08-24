a40b8bd278975e7907ee0c23aaef0a97810a2f55
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-24 17:41:29 +0100

    filter

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const team = [
-  'Joe', 'Theo', 'Bea', 'Dyan', 'Afoo'
+// const team = [
+//   'Joe', 'Theo', 'Bea', 'Dyan', 'Afoo'
+// ]
+
+// function alphabetizeTeams(team) {
+//   return [...team].sort()
+// }
+
+// console.log(alphabetizeTeams(team));
+
+const staff = [
+  {
+    name: 'Wesley',
+    position: 'musician'
+  },
+  {
+    name: 'Tinubu',
+    position: 'pastor'
+  },
+  {
+    name: 'Afolabi',
+    position: 'developer'
+  },
+  ,
+  {
+    name: 'Michael',
+    position: 'developer'
+  },
 ]
 
-function alphabetizeTeams(team) {
-  return [...team].sort()
+function getMusicians(staff) {
+  return staff.filter(staff => staff.position == 'developer')
+  
 }
 
-console.log(alphabetizeTeams(team));
-
+console.log(getMusicians(staff))
